17025e84b06014c24921286084ee83840f0de362
Freeze16 <gnetov.i@mail.ru>
2024-04-13 00:22:30 +0300

    renamed

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DASHA_API_KEY = os.getenv("DASHA_API_KEY")
-DASHA_BASE_ID = os.getenv("BASE_ID")
+DASHA_BASE_ID = os.getenv("DASHA_BASE_ID")
 
 PARTICIPANTS_RAW_CSV = os.getenv("PARTICIPANTS_RAW_CSV")
